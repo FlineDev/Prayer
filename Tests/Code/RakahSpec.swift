3baf39dddcc683d973400692e7340c87e6adfe1b
Cihat Gündüz <CihatGuenduez@posteo.de>
2017-01-17 23:05:21 +0100

    Add basic specs for salah and rakah

--- Tests/Code/RakahSpec.swift
+++ Tests/Code/RakahSpec.swift
@@ -15,9 +15,19 @@ class RakahSpec: QuickSpec {
     override func spec() {
         it("has the correct components count for beginning rakah") {
             let rakah = Rakah(isBeginningOfPrayer: true, includesStandingRecitation: true, includesSittingRecitation: false, isEndOfPrayer: false)
-            let components = rakah.components()
 
-            expect(components.count).to(equal(12))
+            let randomRecitation = "RR"
+            let expectedComponentNames = [
+                "Takbīr", "Opening Supplication", "Ta'awwudh", "The Opening", randomRecitation, "Takbīr", "Ruku", "Straightening Up", "Takbīr",
+                "Sajdah", "Takbīr", "Takbīr", "Sajdah", "Takbīr"
+            ]
+
+            expect(rakah.components().count).to(equal(expectedComponentNames.count))
+            for (i, component) in rakah.components().enumerated() {
+                // skip comparison for random recitations
+                if expectedComponentNames[i] == randomRecitation { continue }
+                expect(component.name).to(equal(expectedComponentNames[i]))
+            }
         }
     }
 }
--- Tests/Code/SalahSpec.swift
+++ Tests/Code/SalahSpec.swift
@@ -13,6 +13,63 @@ import Nimble
 
 class SalahSpec: QuickSpec {
     override func spec() {
-        // TODO: not yet implemented
+        it("initializes correctly with 4 rakat") {
+            let salah = Salah(rakatCount: 4)
+            let randomRecitation = "RR"
+
+            expect(salah.rakat.count).to(equal(4))
+
+            // first rakah
+            var expectedComponentNames = [
+                "Takbīr", "Opening Supplication", "Ta'awwudh", "The Opening", randomRecitation, "Takbīr", "Ruku", "Straightening Up", "Takbīr",
+                "Sajdah", "Takbīr", "Takbīr", "Sajdah", "Takbīr"
+            ]
+
+            expect(salah.rakat[0].components().count).to(equal(expectedComponentNames.count))
+            for (i, component) in salah.rakat[0].components().enumerated() {
+                // skip comparison for random recitations
+                if expectedComponentNames[i] == randomRecitation { continue }
+                expect(component.name).to(equal(expectedComponentNames[i]))
+            }
+
+            // second rakah
+            expectedComponentNames = [
+                "The Opening", randomRecitation, "Takbīr", "Ruku", "Straightening Up", "Takbīr",
+                "Sajdah", "Takbīr", "Takbīr", "Sajdah", "Takbīr", "Tashahhud"
+            ]
+
+            expect(salah.rakat[1].components().count).to(equal(expectedComponentNames.count))
+            for (i, component) in salah.rakat[1].components().enumerated() {
+                // skip comparison for random recitations
+                if expectedComponentNames[i] == randomRecitation { continue }
+                expect(component.name).to(equal(expectedComponentNames[i]))
+            }
+
+            // third rakah
+            expectedComponentNames = [
+                "The Opening", "Takbīr", "Ruku", "Straightening Up", "Takbīr",
+                "Sajdah", "Takbīr", "Takbīr", "Sajdah", "Takbīr"
+            ]
+
+            expect(salah.rakat[2].components().count).to(equal(expectedComponentNames.count))
+            for (i, component) in salah.rakat[2].components().enumerated() {
+                // skip comparison for random recitations
+                if expectedComponentNames[i] == randomRecitation { continue }
+                expect(component.name).to(equal(expectedComponentNames[i]))
+            }
+
+            // fourth rakah
+            expectedComponentNames = [
+                "The Opening", "Takbīr", "Ruku", "Straightening Up", "Takbīr",
+                "Sajdah", "Takbīr", "Takbīr", "Sajdah", "Takbīr", "Tashahhud", "Salatul-'Ibrahimiyyah", "Rabbanagh", "Salâm", "Salâm"
+            ]
+
+            expect(salah.rakat[3].components().count).to(equal(expectedComponentNames.count))
+            for (i, component) in salah.rakat[3].components().enumerated() {
+                // skip comparison for random recitations
+                if expectedComponentNames[i] == randomRecitation { continue }
+                expect(component.name).to(equal(expectedComponentNames[i]))
+            }
+        }
     }
 }
