71b202feaa14431b976c2b270e1fd3db2e64b8c0
Cihat Gündüz <commits@cihatguenduez.de>
2022-04-10 17:09:09 +0200

    Update dependencies + fix build + improve English UI text

---
+++ App/Sources/Globals/Extensions/DefaultsSerializableExt.swift
@@ -0,0 +1,13 @@
+import Foundation
+import SwiftyUserDefaults
+
+// NOTE: Workaround for a compiler bug, see here: https://github.com/sunshinejr/SwiftyUserDefaults/issues/285#issuecomment-1081226108
+extension DefaultsSerializable where Self: Codable {
+  public typealias Bridge = DefaultsCodableBridge<Self>
+  public typealias ArrayBridge = DefaultsCodableBridge<[Self]>
+}
+
+extension DefaultsSerializable where Self: RawRepresentable {
+  typealias Bridge = DefaultsRawRepresentableBridge<Self>
+  typealias ArrayBridge = DefaultsRawRepresentableArrayBridge<[Self]>
+}
